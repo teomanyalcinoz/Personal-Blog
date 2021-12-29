824d959070375185103cefa44d9a3b0ea57b3c8b
Teoman Yalcinoz <teoyalcinoz@gmail.com>
2021-12-29 17:01:40 +0300

    anasayfa

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
 import '../styles/globals.css'
-
+import Header from '../components/header'
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className='antialiased text-gray-700'>
+      <Header></Header>
+      <main className="mt-6 mb-20">
+        <Component { ...pageProps } />
+      </main>
+
+    </div>
+  )
+
+
 }
 
 export default MyApp
