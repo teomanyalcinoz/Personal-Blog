70b7669678ca9ac150004d41426eb09596f107ce
Teoman Yalcinoz <teoyalcinoz@gmail.com>
2022-02-10 00:57:59 +0300

    cyberthreatint

--- pages/index.js
+++ pages/index.js
@@ -4,22 +4,19 @@
 function Home() {
   return (
     <>
-      <div className="site-container">
-        <div className="space-y-10">
-          <h1 className="text-4xl absolute font-bold ml-60 mt-10 text-white">Hakkımda</h1>
-          <p className="box-content h-40 w-70 bg-gray-700 "></p>
-          <p className="text-2xl">Merhaba, ben Teoman Yalçinöz.</p>
-          <p className="text-2xl">Bursa'nın Osmangazi ilçesinde doğdum.Yalova'da yaşıyorum.Yalova Şehit Osman Altınkuyu Anadolu Lisesi mezunuyum
-            .Şuan hala devam etmekte olduğum Yalova Üniversitesi Bilgisayar Mühendisliği 3. sınıf öğrencisiyim.</p>
-          <p className="text-2xl"> Siber Güvenlik , Network , Web Programlama alanlarında araştırmalar ve çalışmalar yapmaktayım.İleride daha çok Ağ Güvenliği
-            alanında kariyer yapmak istiyorum.Bu hedefime yönelik ise CCNA 200-301 çalışıyorum.Bunların dışında ise şu sıralar Python,Bash ve Vue.js üzerinde çalışmalar yapıyorum.
-            Türkçe kaynakların çoğalması adına da bahsettiğim bu alanlarda edindiğim bilgileri sizlerle paylaşmaya çalışacağım.
-          </p>
-        </div>
-      </div>
-      <div className="site-container mt-20">
+      <div className="site-container space-y-5">
+        <h1 className="text-4xl absolute font-bold ml-60 mt-10 text-white">Hakkımda</h1>
+        <p className="box-content h-40 w-70 bg-gray-700 "></p>
+        <p className="text-2xl">Merhaba, ben Teoman Yalçinöz.</p>
+        <p className="text-2xl">Bursa'nın Osmangazi ilçesinde doğdum.Yalova'da yaşıyorum.Yalova Şehit Osman Altınkuyu Anadolu Lisesi mezunuyum
+          .Şuan hala devam etmekte olduğum Yalova Üniversitesi Bilgisayar Mühendisliği 3. sınıf öğrencisiyim.</p>
+        <p className="text-2xl"> Siber Güvenlik , Network , Web Programlama alanlarında araştırmalar ve çalışmalar yapmaktayım.İleride daha çok Ağ Güvenliği
+          alanında kariyer yapmak istiyorum.Bu hedefime yönelik ise CCNA 200-301 çalışıyorum.Bunların dışında ise şu sıralar Python,Bash ve Vue.js üzerinde çalışmalar yapıyorum.
+          Türkçe kaynakların çoğalması adına da bahsettiğim bu alanlarda edindiğim bilgileri sizlerle paylaşmaya çalışacağım.
+        </p>
         <img src="/secc.jpg" alt="security"></img>
       </div>
+
     </>
 
   )
