11ec3ec23465c24c95f7c35dbd5078843f42dae2
Teoman Yalcinoz <teoyalcinoz@gmail.com>
2022-02-14 17:46:15 +0300

    tcpdump

--- pages/index.js
+++ pages/index.js
@@ -9,12 +9,14 @@ function Home() {
 
         <div className="space-y-10">
           <article>
-            <p className="text-2xl">Merhaba, ben Teoman Yalçinöz.</p>
-            <p className="text-2xl">Bursa'nın Osmangazi ilçesinde doğdum.Yalova'da yaşıyorum.Yalova Şehit Osman Altınkuyu Anadolu Lisesi mezunuyum
-              .Şuan hala devam etmekte olduğum Yalova Üniversitesi Bilgisayar Mühendisliği 3. sınıf öğrencisiyim.</p>
-            <p className="text-2xl"> Siber Güvenlik , Network , Web Programlama alanlarında araştırmalar ve çalışmalar yapmaktayım.İleride daha çok Ağ Güvenliği
-              alanında kariyer yapmak istiyorum.Bu hedefime yönelik ise CCNA 200-301 çalışıyorum.Bunların dışında ise şu sıralar Python,Bash ve Vue.js üzerinde çalışmalar yapıyorum.
-              Türkçe kaynakların çoğalması adına da bahsettiğim bu alanlarda edindiğim bilgileri sizlerle paylaşmaya çalışacağım.
+            <p className="text-xl">Merhaba, ben Teoman Yalçinöz.Yalova'da yaşıyorum.Şuan halen devam etmekte olduğum Yalova Üniversitesi Bilgisayar Mühendisliği 3. sınıf öğrencisiyim.
+              Üniversite öğrenimim boyunca siber güvenliğin her alanını merak ettim ve araştırdım.
+              Bu merakım siber güvenlik alanında pek çok eğitime ve konferansa katılım sağlamama,siber güvenlik kulüpleri ile iç içe olup aktif rol almama neden oldu.
+              Şuanda Yalova Üniversitesi Siber Güvenlik ve Teknoloji Kulübünde yönetim kurulu başkanlığı ve gönüllü eğitmenlik görevini sürdürüyorum.Siber güvenliğin pek çok alanı hakkında araştırıp fikir
+              sahibi olduktan sonra kariyerimi siber güvenlik mavi takım alanına yöneltmek istediğime karar verdim.SIEM,SOC,ağ yönetimi ve güvenliği,CCNA 200-301,cyber threat hunting,incident response konularına ilgi duymaktayım.
+              Şuanda da kendimi hali hazırda bu alanlarda geliştirmek üzere çalışmalarıma devam ediyorum.Bunların yanısıra Python,Bash ve Vue.js ile ilgileniyorum.Türkçe kaynakların çoğalması adına da bahsettiğim bu alanlarda edindiğim bilgileri sizlerle paylaşmaya çalışacağım.</p>
+            <p className="text-xl">
+
             </p>
             <img className="mt-10" src="/secc.jpg" alt="security"></img>
           </article>
