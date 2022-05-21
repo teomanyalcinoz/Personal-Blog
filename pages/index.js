b4c5b1b581fae992f7ca9fdfed44fd6c96ce76ed
Teoman Yalcinoz <teoyalcinoz@gmail.com>
2022-05-21 18:58:27 +0300

    3yazıvecv

--- pages/index.js
+++ pages/index.js
@@ -9,16 +9,15 @@ function Home() {
 
         <div className="space-y-10">
           <article>
-            <p className="text-xl">Merhaba, ben Teoman Yalçinöz.Yalova'da yaşıyorum.Şuan halen devam etmekte olduğum Yalova Üniversitesi Bilgisayar Mühendisliği 3. sınıf öğrencisiyim.
-              Üniversite öğrenimim boyunca siber güvenliğin her alanını merak ettim ve araştırdım.
-              Bu merakım siber güvenlik alanında pek çok eğitime ve konferansa katılım sağlamama,siber güvenlik kulüpleri ile iç içe olup aktif rol almama neden oldu.
-              Şuanda Yalova Üniversitesi Siber Güvenlik ve Teknoloji Kulübünde yönetim kurulu başkanlığı ve gönüllü eğitmenlik görevini sürdürüyorum.Siber güvenliğin pek çok alanı hakkında araştırıp fikir
-              sahibi olduktan sonra kariyerimi siber güvenlik mavi takım alanına yöneltmek istediğime karar verdim.SIEM,SOC,ağ yönetimi ve güvenliği,CCNA 200-301,cyber threat hunting,incident response konularına ilgi duymaktayım.
-              Şuanda da kendimi hali hazırda bu alanlarda geliştirmek üzere çalışmalarıma devam ediyorum.Bunların yanısıra Python,Bash ve Vue.js ile ilgileniyorum.Türkçe kaynakların çoğalması adına da bahsettiğim bu alanlarda edindiğim bilgileri sizlerle paylaşmaya çalışacağım.</p>
-            <p className="text-xl">
-
+            <p>[TR]</p>
+            <p className="text-xl">Merhaba, ben Teoman Yalçinöz. Yalova'da yaşıyorum. Şuan halen devam etmekte olduğum Yalova Üniversitesi Bilgisayar Mühendisliği 3. sınıf öğrencisiyim. Üniversite öğrenimim boyunca siber güvenliğin her alanını merak ettim ve araştırdım. Bu araştırmalarımın ardından savunmada olmayı sevdiğimi fark ettim. Siber güvenliğe merakım bu alanda pek çok eğitime ve konferansa katılım sağlamama, siber güvenlik kulüpleri ile iç içe olup aktif rol almama neden oldu. Şuanda da Yalova Üniversitesi Siber Güvenlik ve Teknoloji Kulübünde yönetim kurulu başkanlığı ve gönüllü eğitmenlik görevini sürdürüyorum. Siber güvenliğin pek çok alanı hakkında araştırıp fikir sahibi olduktan sonra kariyerimi siber güvenlik mavi takım alanına yöneltmek istediğime karar verdim. SIEM, SOC, Cyber Threat Intelligence, CCNA 200-301, Cyber Threat Hunting,Incident Response konularına ilgi duymaktayım. Şuanda da kendimi hali hazırda bu alanlarda geliştirmek üzere çalışmalarıma devam ediyorum.Bunların yanısıra Python, C ve Web Programlama ile ilgileniyorum.Ayrıca Bash scripting ve Go programlama dili için kendimi geliştirmek istiyorum. BGA Bilgi Güvenliği şirketinde Siber Tehdit İstibaratı alanında 2 aylık bir staj tecrübesi yaşadım. Şuanda da kariyerime yarı zamanlı bi şekilde SOCRadar® Extended Threat Intelligence şirketinde Junior Siber Tehdit İstihbaratı Analisti olarak devam ediyorum. Türkçe kaynakların çoğalması adına da bahsettiğim bu alanlarda edindiğim bilgileri sizlerle paylaşmaya çalışacağım.
             </p>
-            <img className="mt-10" src="/secc.jpg" alt="security"></img>
+
+            <img className="mt-10 mb-10" src="/secc.jpg" alt="security"></img>
+            <p>[EN]</p>
+            <p className="text-xl">
+              Hi, I'm Teoman Yalçinöz. I live in Yalova. I am a 3rd year Computer Engineering student at Yalova University, which I am currently attending. During my university education, I wondered and researched every field of cyber security. After this research, I realized that I like to be on the defensive. My interest in cyber security has led me to attend many trainings and conferences in this field, and to take an active role by being intertwined with cyber security clubs. Currently, I continue to serve as the chairman of the board of directors and volunteer trainer at Yalova University Cyber ​​Security and Technology Club. After researching and having an idea about many areas of cyber security, I decided that I wanted to direct my career to the field of cyber security blue team. I am interested in SIEM, SOC, Cyber ​​Threat Intelligence, CCNA 200-301, Cyber ​​Threat Hunting, Incident Response. Currently, I continue to work to improve myself in these areas. Besides, I am interested in Python, C and Web Programming. I also want to improve myself for Bash scripting and Go programming language. I had a 2-month internship experience in the field of Cyber ​​Threat Intelligence at BGA Information Security company. Currently, I continue my career as a Junior Cyber ​​Threat Intelligence Analyst at SOCRadar® Extended Threat Intelligence company part time. In order to increase the number of Turkish resources, I will try to share with you the information I have gained in these areas.
+            </p>
           </article>
         </div>
 
